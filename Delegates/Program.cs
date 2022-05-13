36dcf8667709d5cae3045f329ede86d76c3c3457
ssanthu81 <101133033+ssanthu81@users.noreply.github.com>
2022-05-13 09:01:52 -0500

    Add files via upload

---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public void Addnum(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+    public static string hello (string name ){
+
+        return "helloo" + name;
+
+}
+
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.Addnum(25 , 24);
+        string st = Program.hello("shiva");
+        Console.WriteLine(st);
+
+        Console.ReadLine();
+
+    }
+}
+
+
